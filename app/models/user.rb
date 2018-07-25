b45379e25a78fa05f8d41998b1c162eca0dd8178
Ahmed <ahmahm@hotmail.co.jp>
2018-07-26 00:57:48 +0200

    FINAL TA M

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 has_many :articles
-has_many :commentaires
+has_many :commentaires, through: :articles
 has_many :likes
+has_many :commentaires
+#has_many :likes, through: :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,38 @@
 require 'faker'
 
 
-USERS
+#USERS
 10.times do |x|
-    user = User.create(id: "#{x+1}", first_name: Faker::Name.first_name ,last_name: Faker::Name.middle_name, email: Faker::Internet.email)
+    user = User.create(first_name: Faker::Name.first_name ,last_name: Faker::Name.middle_name, email: Faker::Internet.email)
 end
 
 
-ARTICLES
+#ARTICLES
 10.times do |x|
-    articles =  Article.create(id: "#{x+1}", title: Faker::Hipster.word ,content: Faker::Lorem.paragraph, user_id: "#{x+1}")
+    articles =  Article.create(title: Faker::Hipster.word ,content: Faker::Lorem.paragraph, user_id: "#{x+1}")
 end
 
 
-CATEGORIES
+#CATEGORIES
 10.times do |x|
-    categories = Categorie.create(id: "#{x+1}", categorie: Faker::Hipster.word, article_id: "#{x+1}")
+    categories = Categorie.create(categorie: Faker::Hipster.word, article_id: "#{x+1}")
 end
 
 
 
-COMMENTAIRES
+#COMMENTAIRES
 15.times do |x|
-    commentaires = Commentaire.create(id: "#{x+1}", texte: Faker::Lorem.paragraph, article_id: "#{x+1}", user_id: "#{x+1}")
+	if x<=10
+    	commentaires = Commentaire.create(texte: Faker::Lorem.paragraph, article_id: "#{x+1}", user_id: "#{x+1}")
+    else x = 0
+    end
 end
 
-like
+#like
 15.times do |x| 
-    likes = Like.create(user_id: "#{x+1}", article_id: "#{x+1}")
+	if x<=10
+    	likes = Like.create(article_id: "#{x+1}", user_id: "#{x+1}")
+	else x = 0
+	end
 end
 
